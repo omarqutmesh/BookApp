d2696b0decf4e8149b3b2de16806964f2223b74f
Omar Emad <omarqutmesh@gmail.com>
2026-09-21 15:46:33 +0300

    fix errors

--- BookApp/src/app/app.routes.ts
+++ BookApp/src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { BookList } from './components/BookList/book-list';
 import { BookDetails } from './components/BookDetails/book-details';
 import { NotFound } from './components/NotFound/not-found';
 export const routes: Routes = [
-  { path: '', component: BookList },
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: 'books', component: BookList },
   { path: 'book/:id', component: BookDetails },
   {
     path: '**',
