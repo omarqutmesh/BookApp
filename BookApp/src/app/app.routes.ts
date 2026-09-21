11f715bef0d02966be705a6360ae8efdb5e72242
Omar Emad <omarqutmesh@gmail.com>
2026-09-21 15:25:20 +0300

    Add NotFound Component with routing and scss

--- BookApp/src/app/app.routes.ts
+++ BookApp/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { BookList } from './components/BookList/book-list';
 import { BookDetails } from './components/BookDetails/book-details';
-
+import { NotFound } from './components/NotFound/not-found';
 export const routes: Routes = [
   { path: '', component: BookList },
   { path: 'book/:id', component: BookDetails },
+  {
+    path: '**',
+    component: NotFound,
+  },
 ];
---
+++ BookApp/src/app/components/NotFound/not-found.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'not-found',
+  templateUrl: './not-found.html',
+  standalone: true,
+})
+export class NotFound {}
